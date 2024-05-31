a42a18d4806e2a39aac2967a73a85cabc1b3d68d
brenner.souza <brenner.souza@gmail.com>
2024-05-31 16:29:24 -0300

    incluindo busca lançamentos

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -4,6 +4,7 @@ import br.com.alura.screenmatch.dto.SerieDTO;
 import br.com.alura.screenmatch.services.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,6 +28,11 @@ public class SerieController {
 
     @GetMapping("/lancamentos")
     public List<SerieDTO> obterLancamentos(){
-        return servico.obterTop5Series();
+        return servico.obterLancamentos();
+    }
+
+    @GetMapping("/{id}")
+    public SerieDTO obterPorId(@PathVariable Long id){
+        return servico.buscarPorId(id);
     }
 }
--- src/main/java/br/com/alura/screenmatch/services/SerieService.java
+++ src/main/java/br/com/alura/screenmatch/services/SerieService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -22,6 +23,10 @@ public class SerieService {
         return converteDados(repositorio.findTop5ByOrderByAvaliacaoDesc());
     }
 
+    public List<SerieDTO> obterLancamentos() {
+        return converteDados(repositorio.encontrarSeriesAtualizadasRecentes());
+    }
+
     private List<SerieDTO>  converteDados(List<Serie> series){
         return series.stream()
                 .map(s -> new SerieDTO(s.getId(),
@@ -34,5 +39,22 @@ public class SerieService {
                         s.getSinopse()))
                 .collect(Collectors.toList());
 
+    }
+
+    public SerieDTO buscarPorId(Long id) {
+        Optional<Serie> serie =repositorio.findById(id);
+        if (serie.isPresent()){
+            Serie s = serie.get();
+            return new SerieDTO(s.getId(),
+                    s.getTitulo(),
+                    s.getTotalTemporadas(),
+                    s.getAvaliacao(),
+                    s.getGenero(),
+                    s.getAtores(),
+                    s.getPoster(),
+                    s.getSinopse());
+        }else {
+            return null;
+        }
     }
 }
